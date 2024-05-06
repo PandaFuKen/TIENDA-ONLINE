246e1fbdd8122ed6bb13ca8759cc1a653ed63c67
PandaFuKen <zaletagames@hotmail.com>
2024-05-06 07:31:41 -0600

    avance

--- Tablas/productos.php
+++ Tablas/productos.php
@@ -19,3 +19,31 @@ include("");
         </thead>
     </table>
 </div>
+
+
+<?php
+require_once "../config/conexion.php";
+
+if (isset($_POST)) {
+    if (!empty($_POST)) {
+        $nombre = $_POST['nombre'];
+        $cantidad = $_POST['cantidad'];
+        $descripcion = $_POST['descripcion'];
+        $precio_rebajado = $_POST['precio_rebajado'];
+        $categoria = $_POST['categoria'];
+        $img = $_FILES['foto'];
+        $name = $img['name'];
+        $tmpname = $img['tmp_name'];
+        $fecha = date("YmdHis");
+        $foto = $fecha . ".jpg";
+        $destino = "../assets/img/" . $foto;
+        $query = mysqli_query($conexion, "INSERT INTO productos(nombre, descripcion, precio_rebajado, cantidad, imagen, id_categoria) VALUES ('$nombre', '$descripcion', '$precio_rebajado', $cantidad, '$foto', $categoria)");
+        if ($query) {
+            if (move_uploaded_file($tmpname, $destino)) {
+                header('Location: productos.php');
+            }
+        }
+    }
+}
+<!--
+<td><img class="img-thumbnail" src="../assets/img/<?php echo $data['imagen']; ?>" width="50"></td> -->
--- Views/vistacategoria.php
+++ Views/vistacategoria.php
@@ -2,18 +2,22 @@
 
 <?php
 include("../php/conexion.php");
+require $_SERVER['DOCUMENT_ROOT'].'/PHP/conexion.php'; 
 
-$sql = "SELECT * FROM categoria";
-$result = mysqli_query($conexion, $sql);
+// Usar solo PDO para la conexión
+$db = Database::connect();
+$categorias = $db->query('SELECT `id_categoria`, `nombre` FROM `categorias` ORDER BY name ASC');
+$items = $db->query('SELECT items.id, items.name, items.description, items.price, items.image,items.opciones,items.extras, categorias.name AS category FROM items LEFT JOIN categorias ON items.category = categorias.id ORDER BY items.id DESC');
+
 ?>
+
 <div class="contenedor-categorias">
   <h2>Todas las categorías:</h2>
   <div class="flex-container">
-    <?php while ($fila = mysqli_fetch_array($result)) { ?>
+    <?php while ($fila = $categorias->fetch(PDO::FETCH_ASSOC)) { ?>
       <div class="circle">
         <!-- Mostrar la información de la categoría -->
-        
-        <a href="#"><p><?php echo $fila['nombre']; ?></p></a>
+        <a href="#"><p><?php echo $fila['name']; ?></p></a>
       </div>
     <?php } ?>
   </div>
